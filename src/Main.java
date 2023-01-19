f84969ae3c99879b3aeda71e39c2183308739536
Axekill <andrei3.ru@ya.ru>
2023-01-19 13:55:36 +0300

    шаблон на 8 задач

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println("задача 1");
+    }
+
+    public static void task2() {
+        System.out.println("задача 2");
+    }
+
+    public static void task3() {
+        System.out.println("задача 3");
+    }
+
+    public static void task4() {
+        System.out.println("задача 4");
+    }
+
+    public static void task5() {
+        System.out.println("задача 5");
+    }
+
+    public static void task6() {
+        System.out.println("задача 6");
+    }
+
+    public static void task7() {
+        System.out.println("задача 7");
+    }
+
+    public static void task8() {
+        System.out.println("задача 8");
+    }
+
+}
